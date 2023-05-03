6b53d8766104094ecd6cd346b54d4d76bc710ed9
PopovAV <PopovAV@ekassir.com>
2023-05-03 19:48:14 +0300

    fix mailto

--- pages/api/notification.ts
+++ pages/api/notification.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import webPush from 'web-push'
 
 webPush.setVapidDetails(
-  `mailto:{process.env.WEB_PUSH_EMAIL}`,
+  `mailto:${process.env.WEB_PUSH_EMAIL}`,
   process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY??"",
   process.env.WEB_PUSH_PRIVATE_KEY??""
 )
